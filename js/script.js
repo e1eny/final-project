2b7e926e59f01d7138dbe7597e440315c10382db
Elene Khunashvili <khunashvilielene@gmail.com>
2021-08-25 22:32:03 +0400

    add Rosa Team section with logos.

--- js/script.js
+++ js/script.js
@@ -61,20 +61,3 @@ function isElementInViewport(el) {
   );
 }
 
-
-// $('.slider span.next').click(function() {
-//   $current = $(this).siblings('img.active');
-//   $next = $current.next('img');
-//   if ($next.length != 0) {
-//     $current.removeClass('active');
-//     $next.addClass('active');
-//   }
-// })
-// $('.slider span.prev').click(function() {
-//   $current = $(this).siblings('img.active');
-//   $prev = $current.prev('img');
-//   if ($prev.length != 0) {
-//     $current.removeClass('active');
-//     $prev.addClass('active');
-//   }
-// })
